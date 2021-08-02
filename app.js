e4d0323ce153c51a5aa2aebbec51c36b72f9e1dd
billybep <billy.pesoth@gmail.com>
2021-08-02 13:13:57 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+if (process.env.NODE_ENV !== "production") require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const router = require('./routes')
 
 // app.get('/', (req, res) => res.send('test backend'))
